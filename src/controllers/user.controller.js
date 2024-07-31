944b3b5f4c12c9f7d2e0ba886f31755563484fdd
Nikhil Rathore <nikhilrathore52297@gmail.com>
2024-07-31 23:18:04 +0530

    Add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -417,6 +417,64 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
 
 })
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    // req.user._id when we do this we get a string , as we are using mongoose it automatically convert the string to mongoDB id
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId((req.user._id))
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup:{
+                           from: "users",
+                           localField: "owner",
+                           foreignField:"_id",
+                           as: "owner", 
+                           pipeline:[
+                            {
+                                $project:{
+                                    fullName: 1,
+                                    username:1,
+                                    avatar: 1,
+                                }
+                            }
+                           ]
+                        }
+
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
+
+
+
 export { 
     registerUser ,
     loginUser,
@@ -427,6 +485,7 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 
 }
